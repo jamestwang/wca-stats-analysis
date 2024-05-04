04c3ccbb19729e286ed7bd00c14719e762abda15
James Wang <james.t.wang2003@gmail.com>
2024-05-04 13:49:04 -0400

    this is the point where i give up on writing good code and just say fuck it and do whatever

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 import datetime as dt
 import time as t
 
+ONLY333 = False
+
 start_time = t.time()
 # Import all results
 results_df = pd.read_csv("WCA_export_Results.tsv", sep="\t")
@@ -37,27 +39,54 @@ events = results_df["eventId"].unique()
 event_dfs = [results_df[results_df["eventId"] == event] for event in events]
 print(f"Event splitting took {t.time() - start_time} seconds.")
 
+# all best singles
 start_time = t.time()
-# plot each event
-for i, event_df in enumerate(event_dfs):
-    event_id = events[i]
-    filename = f"raw_scatterplot_{event_id}.png"
+if (not ONLY333):
+    # plot each event
+    for i, event_df in enumerate(event_dfs):
+        event_id = events[i]
+        filename = f"raw_best_single_scatterplot_{event_id}.png"
+        fig, ax = plt.subplots(figsize=(8, 6))
+        sns.scatterplot(data=event_df, x='date', y='best', ax=ax)
+        ax.set_title(f"Scatter Plot for {event_id}")
+        plt.savefig("Raw_Best_Single_Scatterplots/" + filename)
+        plt.close()
+        print(f"Plotted {len(event_df)} {event_id} best solves.")
+else:
+    event_id = "333"
+    event_df = event_dfs[0]
+    filename = f"raw_best_single_scatterplot_{event_id}.png"
     fig, ax = plt.subplots(figsize=(8, 6))
     sns.scatterplot(data=event_df, x='date', y='best', ax=ax)
     ax.set_title(f"Scatter Plot for {event_id}")
-    plt.savefig("Raw_Scatterplots/" + filename)
+    plt.savefig("Raw_Best_Single_Scatterplots/" + filename)
     plt.close()
+    print(f"Plotted {len(event_df)} {event_id} best solves.")
 print(f"Raw plotting took {t.time() - start_time} seconds.")
 
+# nonzero best singles
 start_time = t.time()
 # plot each event
-for i, event_df in enumerate(event_dfs):
-    event_id = events[i]
-    filename = f"nonzero_raw_scatterplot_{event_id}.png"
+if(not ONLY333):
+    for i, event_df in enumerate(event_dfs):
+        event_id = events[i]
+        filename = f"nonzero_raw_best_single_scatterplot_{event_id}.png"
+        fig, ax = plt.subplots(figsize=(8, 6))
+        event_df_filtered = event_df[event_df['best'] > 0]
+        sns.scatterplot(data=event_df_filtered, x='date', y='best', ax=ax)
+        ax.set_title(f"Scatter Plot for {event_id}")
+        plt.savefig("Nonzero_Raw_Best_Single_Scatterplots/" + filename)
+        plt.close()
+        print(f"Plotted {len(event_df_filtered)} {event_id} best nonzero completed solves.")
+else:
+    event_id = "333"
+    event_df = event_dfs[0]
+    filename = f"nonzero_raw_best_single_scatterplot_{event_id}.png"
     fig, ax = plt.subplots(figsize=(8, 6))
     event_df_filtered = event_df[event_df['best'] > 0]
     sns.scatterplot(data=event_df_filtered, x='date', y='best', ax=ax)
     ax.set_title(f"Scatter Plot for {event_id}")
-    plt.savefig("Nonzero_Raw_Scatterplots/" + filename)
+    plt.savefig("Nonzero_Raw_Best_Single_Scatterplots/" + filename)
     plt.close()
+    print(f"Plotted {len(event_df_filtered)} {event_id} best nonzero completed solves.")
 print(f"Nonzero raw plotting took {t.time() - start_time} seconds.")
